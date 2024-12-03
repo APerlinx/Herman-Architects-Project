82db881d4043ba9aef6640d0ed10382b9653706b
APerlinx <133282938+APerlinx@users.noreply.github.com>
2024-12-03 17:21:23 +0200

    Finish List and gird view for mobile screens

--- src/features/ProjectsPage/ListView.jsx
+++ src/features/ProjectsPage/ListView.jsx
@@ -1,30 +1,28 @@
 function ListView({ projects }) {
-  const textDesign = 'text-base font-semibold text-stone-600';
+  const textDesign = 'text-base font-semibold text-stone-600 py-1 md:py-0s';
   return (
     <div className="space-y-8">
       {projects.map((project, index) => (
         <div
           key={index}
-          className="flex cursor-pointer items-center justify-between gap-8 border-b border-stone-300 pb-4"
+          className=" grid cursor-pointer grid-cols-[2fr_1fr_1fr] items-center gap-8 border-b border-stone-300 pb-4"
         >
           {/* Image */}
-          <img
-            src={project.image}
-            alt={project.title}
-            className="hidden h-32 w-48 object-cover sm:inline-block"
-          />
+          <div className="flex items-center gap-4">
+            <img
+              src={project.image}
+              alt={project.title}
+              className="hidden h-32 w-48 object-cover custom-sm:inline-block"
+            />
 
-          {/* Title, Year, and Place */}
-          <div className="flex w-full items-center justify-between">
-            {/* Title */}
-            <h3 className="text-xl font-bold text-stone-800 transition-colors duration-300 group-hover:text-yellow-500">
+            <h3 className="py-1.5 text-xl font-bold text-stone-800 transition-colors duration-300 group-hover:text-yellow-500">
               {project.title}
             </h3>
-
-            {/* Year and Place */}
-            <p className={textDesign}>{project.place}</p>
-            <p className={textDesign}> הושלם ב- {project.year}</p>
           </div>
+
+          {/* Year and Place */}
+          <p className={textDesign}>{project.place}</p>
+          <p className={textDesign}> הושלם ב- {project.year}</p>
         </div>
       ))}
     </div>
